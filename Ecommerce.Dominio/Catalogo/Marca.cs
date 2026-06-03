ac32013769a32f539e4023d463834cdaa0477987
Maximo Gomer <maximogomer02@gmail.com>
2026-06-03 19:00:05 -0300

    Agrego clase Marca en dominio

---
+++ Ecommerce.Dominio/Catalogo/Marca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Dominio.Catalogo
+{
+    internal class Marca
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}

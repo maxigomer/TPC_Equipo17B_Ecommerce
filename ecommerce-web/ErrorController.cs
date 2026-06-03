7a2f08f893b5949712bec45802dc6d34870739d4
Santino Gerevini <santinogerevini8@gmail.com>
2026-06-03 13:12:36 -0300

    Add ErrorController to handle error views

--- ecommerce-web/ErrorController.cs
+++ ecommerce-web/ErrorController.cs
@@ -1,1 +1,12 @@
+using System.Web.Mvc;
 
+namespace Ecommerce.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

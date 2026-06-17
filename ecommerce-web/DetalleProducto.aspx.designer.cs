fc5f8256c962b672b5745353c37c3c507a8d5adb
Santino Gerevini <santinogerevini8@gmail.com>
2026-06-17 16:26:43 -0300

    Add designer file for DetalleProducto page

---
+++ ecommerce-web/DetalleProducto.aspx.designer.cs
@@ -0,0 +1,13 @@
+namespace ecommerce_web
+{
+    public partial class DetalleProducto
+    {
+        protected global::System.Web.UI.WebControls.Image imgProducto;
+
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        protected global::System.Web.UI.WebControls.Label lblPrecio;
+
+        protected global::System.Web.UI.WebControls.Label lblDescripcion;
+    }
+}

e352ce299babe3ec904b9c2c893760e7fbf10f19
Santino Gerevini <santinogerevini8@gmail.com>
2026-06-21 22:47:55 -0300

    Add search button click event to redirect to Productos

--- ecommerce-web/MasterDesktopAdmin.Master.cs
+++ ecommerce-web/MasterDesktopAdmin.Master.cs
@@ -13,5 +13,10 @@ namespace ecommerce_web
         {
 
         }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+         Response.Redirect("Productos.aspx?buscar=" + txtBuscar.Text);
+        }
     }
 }
